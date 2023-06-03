b97f13a674db31f07399f24502f8074843562d62
carol <carolina.df@outlook.com>
2023-06-02 23:29:04 -0300

    change the url

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -5,5 +5,5 @@ export const api = axios.create({
   endereço de IPV4 aqui. Para saber qual é o seu endereço, basta executar o
   comando "ipconfig" no terminal e copiar o endereço IPV4.
   */
-  baseURL: 'http://192.168.0.14:3333',
+  baseURL: 'http://localhost:3333',
 })
